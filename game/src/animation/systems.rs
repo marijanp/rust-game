0bb60582e877ae7b9f86152b23f0c4ed15aac268
Marijan Petričević <marijan.petricevic94@gmail.com>
2024-10-04 15:55:52 -0500

    game: use ground detection to change the player sprite

--- game/src/animation/systems.rs
+++ game/src/animation/systems.rs
@@ -1,5 +1,5 @@
 use bevy::prelude::*;
-use bevy_rapier2d::prelude::Velocity;
+use bevy_rapier2d::prelude::{KinematicCharacterControllerOutput, Velocity};
 
 use super::components::{AnimationIndices, AnimationTimer};
 use crate::player::components::{Movement, Player};
@@ -27,32 +27,45 @@ type AnimationRelated<'a> = (
     &'a mut Handle<Image>,
     &'a mut Sprite,
     &'a mut AnimationIndices,
+    &'a KinematicCharacterControllerOutput,
 );
 
+const DELTA: f32 = 10.0;
+
 pub fn change_player_animation(mut player: Query<AnimationRelated, With<Player>>) {
-    if let Ok((velocity, textures, mut current_texture, mut sprite, mut animation_indices)) =
-        player.get_single_mut()
+    if let Ok((
+        velocity,
+        textures,
+        mut current_texture,
+        mut sprite,
+        mut animation_indices,
+        character_controller,
+    )) = player.get_single_mut()
     {
-        if velocity.linvel.x == 0. && velocity.linvel.y == 0. {
-            *current_texture = textures.get(&Movement::Idle).unwrap().clone();
-            animation_indices.last = 10;
-        } else {
-            if velocity.linvel.y > 0. {
-                *current_texture = textures.get(&Movement::Jump).unwrap().clone();
-                animation_indices.last = 0;
-            } else if velocity.linvel.y < 0. {
-                animation_indices.last = 0;
-                *current_texture = textures.get(&Movement::Fall).unwrap().clone()
+        //if (-DELTA..=DELTA).contains(&velocity.linvel.y) {
+        if character_controller.grounded {
+            if (-DELTA..=DELTA).contains(&velocity.linvel.x) {
+                *current_texture = textures.get(&Movement::Idle).unwrap().clone();
+                animation_indices.last = 10;
+                info!("idle velocity: {velocity:?}");
             } else {
+                info!("run velocity: {velocity:?}");
                 animation_indices.last = 10;
                 *current_texture = textures.get(&Movement::Run).unwrap().clone();
             }
-            if velocity.linvel.x > 0. {
-                sprite.flip_x = false;
-            }
-            if velocity.linvel.x < 0. {
-                sprite.flip_x = true;
-            }
+        } else if !character_controller.grounded && velocity.linvel.y > DELTA {
+            info!("jump velocity: {velocity:?}");
+            *current_texture = textures.get(&Movement::Jump).unwrap().clone();
+            animation_indices.last = 0;
+        } else if !character_controller.grounded && velocity.linvel.y < -DELTA {
+            info!("fall velocity: {velocity:?}");
+            animation_indices.last = 0;
+            *current_texture = textures.get(&Movement::Fall).unwrap().clone()
+        }
+        if velocity.linvel.x > DELTA {
+            sprite.flip_x = false;
+        } else if velocity.linvel.x < -DELTA {
+            sprite.flip_x = true;
         }
     }
 }
