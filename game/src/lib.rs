a9df26149c9223d04c183b8e151b1f31e8b4060d
Marijan Petričević <marijan.petricevic94@gmail.com>
2024-11-16 20:11:02 -0500

    lib: refactor

--- game/src/lib.rs
+++ game/src/lib.rs
@@ -15,12 +15,14 @@ use crate::player::components::Player;
 use bevy::prelude::*;
 use bevy::render::camera::ScalingMode;
 use bevy::window::PrimaryWindow;
-#[cfg(debug_assertions)]
+#[cfg(feature = "dev")]
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use bevy_rapier3d::prelude::*;
 use bevy_spritesheet_animation::prelude::SpritesheetAnimationPlugin;
 use blenvy::*;
 use leafwing_input_manager::prelude::*;
+
+// App
 
 #[derive(States, Clone, Debug, Copy, PartialEq, Eq, Hash, Default)]
 pub enum AppState {
@@ -30,16 +32,9 @@ pub enum AppState {
     GameOver,
 }
 
-#[derive(States, Clone, Debug, Copy, PartialEq, Eq, Hash, Default)]
-pub enum GameState {
-    #[default]
-    Running,
-    Paused,
-}
-
 /// Runs the game given the cli arguments parameter
 pub fn run(CliArgs { listen_address }: CliArgs) {
-    tracing::info!("Game started {listen_address:?}");
+    info!("Game started {listen_address:?}");
     App::new()
         // default_nearest disables linear texture filtering, we need this because of pixel art
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
@@ -49,13 +44,22 @@ pub fn run(CliArgs { listen_address }: CliArgs) {
         .run();
 }
 
+// Game
+
+#[derive(States, Clone, Debug, Copy, PartialEq, Eq, Hash, Default)]
+pub enum GameState {
+    #[default]
+    Running,
+    Paused,
+}
+
 pub struct GamePlugin;
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
         app.insert_state(GameState::Paused)
             .add_systems(Startup, spawn_camera)
-            .add_plugins(BlenvyPlugin::default())
             .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
+            .add_plugins(BlenvyPlugin::default())
             .add_plugins(InputManagerPlugin::<Input>::default())
             .add_plugins(SpritesheetAnimationPlugin)
             .add_plugins(ui::UiPlugin)
@@ -63,12 +67,14 @@ impl Plugin for GamePlugin {
             .add_plugins(player::PlayerPlugin)
             .add_plugins(fruit::FruitPlugin)
             .add_plugins(enemy::EnemyPlugin)
-            .add_systems(Update, (touch_system, update_camera));
-        #[cfg(debug_assertions)]
+            .add_systems(Update, (touch_system, move_camera));
+        #[cfg(feature = "dev")]
         app.add_plugins(RapierDebugRenderPlugin::default())
             .add_plugins(WorldInspectorPlugin::new());
     }
 }
+
+// Input manager
 
 #[derive(Actionlike, PartialEq, Eq, Hash, Clone, Copy, Debug, Reflect)]
 pub enum Input {
@@ -102,32 +108,36 @@ impl Input {
     }
 }
 
+// Camera
+
 pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
     let window = window_query.get_single().unwrap();
     let width = window.width();
     let height = window.height();
     let physical_width = window.physical_width();
     let physical_height = window.physical_height();
-    info!("logical: {width}x{height}");
-    info!("physical: {physical_width}x{physical_height}");
+    info!("logical window dimensions: {width}x{height}");
+    info!("physical window dimensions: {physical_width}x{physical_height}");
+    let common_transform =
+        Transform::from_xyz(0., 8., 10.).looking_at(Vec3::new(0.0, 4., 0.0), Vec3::Y);
     // light
     commands.spawn(DirectionalLightBundle {
-        transform: Transform::from_xyz(0., 0., 10.).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: common_transform,
         ..default()
     });
     // camera
     commands.spawn(Camera3dBundle {
         projection: OrthographicProjection {
-            scaling_mode: ScalingMode::WindowSize(64.),
+            scaling_mode: ScalingMode::WindowSize(32.),
             ..default()
         }
         .into(),
-        transform: Transform::from_xyz(0., 3.6, 10.).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: common_transform,
         ..default()
     });
 }
 
-fn update_camera(
+fn move_camera(
     mut camera: Query<&mut Transform, (With<Camera3d>, Without<Player>)>,
     player: Query<&Transform, (With<Player>, Without<Camera3d>)>,
     time: Res<Time>,
@@ -153,35 +163,29 @@ fn touch_system(
     touches: Res<Touches>,
     window_query: Query<&Window, With<PrimaryWindow>>,
     mut action_state_query: Query<&mut ActionState<Input>>,
-    player_query: Query<&Transform, With<Player>>,
 ) {
     let window = window_query.get_single().unwrap();
-    if let Ok(player) = player_query.get_single() {
-        if let Ok(mut action_state) = action_state_query.get_single_mut() {
-            if touches
-                .iter()
-                .any(|touch| touch.position().x < player.translation.x)
-            {
-                action_state.press(&Input::Left);
-            } else if touches
-                .iter()
-                .any(|touch| touch.position().x >= player.translation.x)
-            {
-                action_state.press(&Input::Right);
-            }
-            if touches.iter().count() + touches.iter_just_pressed().count() >= 2 {
-                if touches
-                    .iter_just_pressed()
-                    .any(|touch| touch.position().y < window.height() / 2.)
-                {
-                    action_state.press(&Input::Up);
-                } else if touches
-                    .iter_just_pressed()
-                    .any(|touch| touch.position().y >= window.height() / 2.)
-                {
-                    action_state.press(&Input::Down);
-                }
-            }
+    if let Ok(mut action_state) = action_state_query.get_single_mut() {
+        if touches
+            .iter()
+            .any(|touch| touch.position().x < window.width() / 2.)
+        {
+            action_state.press(&Input::Left);
+        } else if touches
+            .iter()
+            .any(|touch| touch.position().x >= window.width() / 2.)
+        {
+            action_state.press(&Input::Right);
+        } else if touches
+            .iter()
+            .any(|touch| touch.position().y < window.height() / 2.)
+        {
+            action_state.press(&Input::Up);
+        } else if touches
+            .iter()
+            .any(|touch| touch.position().y >= window.height() / 2.)
+        {
+            action_state.press(&Input::Down);
         }
     };
 }
