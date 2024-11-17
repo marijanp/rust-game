0e476ae75e3a237eff998716e84831ea5bb2c7e0
Marijan Petričević <marijan.petricevic94@gmail.com>
2024-11-16 20:10:46 -0500

    collider: add mass properties to ColliderBundel

--- game/src/collider.rs
+++ game/src/collider.rs
@@ -1,9 +1,12 @@
 use bevy::prelude::*;
+use bevy_rapier3d::geometry::Collider as RapierCollider;
 use bevy_rapier3d::prelude::*;
 
 #[derive(Clone, Debug, Default, Bundle)]
 pub struct ColliderBundle {
-    pub collider: Collider,
+    // A collider and mass_properties are required for forces and impulses to be applied correctly
+    pub collider: RapierCollider,
+    pub mass_properties: ColliderMassProperties,
     pub rigid_body: RigidBody,
     pub active_events: ActiveEvents,
     pub rotation_constraints: LockedAxes,
