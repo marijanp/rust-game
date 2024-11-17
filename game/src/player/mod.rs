35fcd7bf50506db10386c749ad0b22c1568bd036
Marijan Petričević <marijan.petricevic94@gmail.com>
2024-11-16 20:10:08 -0500

    player: refactor

--- game/src/player/mod.rs
+++ game/src/player/mod.rs
@@ -3,6 +3,7 @@ pub mod systems;
 
 use crate::{AppState, GameState};
 use bevy::prelude::*;
+use blenvy::GltfBlueprintsSet;
 
 pub const PLAYER_WIDTH: f32 = 32.0;
 pub const PLAYER_HEIGHT: f32 = 32.0;
@@ -11,21 +12,25 @@ pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(AppState::InGame), systems::spawn)
-            .add_systems(
-                Update,
-                (
-                    systems::move_player,
-                    systems::player_animation_event,
-                    systems::change_player_animation,
-                )
-                    .chain()
-                    .run_if(in_state(GameState::Running)),
+        app.add_systems(
+            OnEnter(AppState::InGame),
+            systems::spawn.after(GltfBlueprintsSet::AfterSpawn),
+        )
+        .add_systems(
+            Update,
+            (
+                systems::move_player,
+                systems::punch,
+                systems::player_animation_event,
+                systems::flip_player_sprite,
             )
-            .add_systems(
-                Update,
-                systems::collect_fruits.run_if(in_state(GameState::Running)),
-            )
-            .add_systems(OnExit(AppState::InGame), systems::despawn);
+                .chain()
+                .run_if(in_state(GameState::Running)),
+        )
+        .add_systems(
+            Update,
+            systems::collect_fruits.run_if(in_state(GameState::Running)),
+        )
+        .add_systems(OnExit(AppState::InGame), systems::despawn);
     }
 }
--- game/src/player/systems.rs
+++ game/src/player/systems.rs
@@ -4,6 +4,7 @@ use bevy_spritesheet_animation::prelude::*;
 use leafwing_input_manager::prelude::*;
 
 use crate::collider::ColliderBundle;
+use crate::enemy::components::Enemy;
 use crate::fruit::components::Fruit;
 use crate::player::components::{Movement, Player, PlayerBundle};
 use crate::Input;
@@ -19,132 +20,157 @@ pub fn spawn(
     let spritesheet = Spritesheet::new(61, 1);
 
     // Idle
-    let idle_clip = Clip::from_frames(spritesheet.horizontal_strip(0, 0, 12));
-    let idle_clip_id = library.register_clip(idle_clip);
-    let idle_animation = Animation::from_clip(idle_clip_id);
-    let idle_animation_id = library.register_animation(idle_animation);
-    library
-        .name_animation(idle_animation_id, Movement::Idle)
-        .unwrap();
+    let idle_animation_id = library
+        .animation_with_name(Movement::Idle)
+        .unwrap_or_else(|| {
+            let clip = Clip::from_frames(spritesheet.horizontal_strip(0, 0, 12));
+            let clip_id = library.register_clip(clip);
+            let animation = Animation::from_clip(clip_id);
+            let animation_id = library.register_animation(animation);
+            library
+                .name_animation(animation_id, Movement::Idle)
+                .unwrap();
+            animation_id
+        });
 
     // Jump
-    let jump_clip = Clip::from_frames(spritesheet.horizontal_strip(24, 0, 2));
-    let jump_clip_id = library.register_clip(jump_clip);
-    let mut animation = Animation::from_clip(jump_clip_id);
-    animation.set_repetitions(AnimationRepeat::Times(1));
-    let animation_id = library.register_animation(animation);
-    library
-        .name_animation(animation_id, Movement::Jump)
-        .unwrap();
+    if library.animation_with_name(Movement::Jump).is_none() {
+        let jump_clip = Clip::from_frames(spritesheet.horizontal_strip(24, 0, 2));
+        let jump_clip_id = library.register_clip(jump_clip);
+        let mut animation = Animation::from_clip(jump_clip_id);
+        animation.set_repetitions(AnimationRepeat::Times(1));
+        let animation_id = library.register_animation(animation);
+        library
+            .name_animation(animation_id, Movement::Jump)
+            .unwrap();
+    }
 
     // Fall
-    let fall_clip = Clip::from_frames(spritesheet.horizontal_strip(26, 0, 2));
-    let fall_clip_id = library.register_clip(fall_clip);
-    let mut animation = Animation::from_clip(fall_clip_id);
-    animation.set_repetitions(AnimationRepeat::Times(1));
-    let animation_id = library.register_animation(animation);
-    library
-        .name_animation(animation_id, Movement::Fall)
-        .unwrap();
+    if library.animation_with_name(Movement::Fall).is_none() {
+        let fall_clip = Clip::from_frames(spritesheet.horizontal_strip(26, 0, 2));
+        let fall_clip_id = library.register_clip(fall_clip);
+        let mut animation = Animation::from_clip(fall_clip_id);
+        animation.set_repetitions(AnimationRepeat::Times(1));
+        let animation_id = library.register_animation(animation);
+        library
+            .name_animation(animation_id, Movement::Fall)
+            .unwrap();
+    }
 
     // Run
-    let walk_clip = Clip::from_frames(spritesheet.horizontal_strip(28, 0, 16));
-    let walk_clip_id = library.register_clip(walk_clip);
-    let animation = Animation::from_clip(walk_clip_id);
-    let animation_id = library.register_animation(animation);
-    library
-        .name_animation(animation_id, Movement::Walk)
-        .unwrap();
+    if library.animation_with_name(Movement::Walk).is_none() {
+        let walk_clip = Clip::from_frames(spritesheet.horizontal_strip(28, 0, 16));
+        let walk_clip_id = library.register_clip(walk_clip);
+        let animation = Animation::from_clip(walk_clip_id);
+        let animation_id = library.register_animation(animation);
+        library
+            .name_animation(animation_id, Movement::Walk)
+            .unwrap();
+    }
 
     // Jab
-    let jab_clip = Clip::from_frames(spritesheet.horizontal_strip(44, 0, 3));
-    let jab_clip_id = library.register_clip(jab_clip);
-    let mut jab_animation = Animation::from_clip(jab_clip_id);
-    jab_animation
-        .set_repetitions(AnimationRepeat::Times(1))
-        .set_duration(AnimationDuration::PerRepetition(200));
-    let jab_animation_id = library.register_animation(jab_animation);
-    library
-        .name_animation(jab_animation_id, Movement::Jab)
-        .unwrap();
+    if library.animation_with_name(Movement::Jab).is_none() {
+        let jab_clip = Clip::from_frames(spritesheet.horizontal_strip(44, 0, 3));
+        let jab_clip_id = library.register_clip(jab_clip);
+        let mut jab_animation = Animation::from_clip(jab_clip_id);
+        jab_animation
+            .set_repetitions(AnimationRepeat::Times(1))
+            .set_duration(AnimationDuration::PerRepetition(200));
+        let jab_animation_id = library.register_animation(jab_animation);
+        library
+            .name_animation(jab_animation_id, Movement::Jab)
+            .unwrap();
+    }
 
     // Hook
-    let hook_clip = Clip::from_frames(spritesheet.horizontal_strip(47, 0, 5));
-    let hook_clip_id = library.register_clip(hook_clip);
-    let mut hook_animation = Animation::from_clip(hook_clip_id);
-    hook_animation
-        .set_repetitions(AnimationRepeat::Times(1))
-        .set_duration(AnimationDuration::PerRepetition(200));
-    let hook_animation_id = library.register_animation(hook_animation);
-    library
-        .name_animation(hook_animation_id, Movement::Hook)
-        .unwrap();
+    if library.animation_with_name(Movement::Hook).is_none() {
+        let hook_clip = Clip::from_frames(spritesheet.horizontal_strip(47, 0, 5));
+        let hook_clip_id = library.register_clip(hook_clip);
+        let mut hook_animation = Animation::from_clip(hook_clip_id);
+        hook_animation
+            .set_repetitions(AnimationRepeat::Times(1))
+            .set_duration(AnimationDuration::PerRepetition(200));
+        let hook_animation_id = library.register_animation(hook_animation);
+        library
+            .name_animation(hook_animation_id, Movement::Hook)
+            .unwrap();
+    }
 
     // Uppercut
-    let uppercut_clip = Clip::from_frames(spritesheet.horizontal_strip(55, 0, 6));
-    let uppercut_clip_id = library.register_clip(uppercut_clip);
-    let mut animation = Animation::from_clip(uppercut_clip_id);
-    animation
-        .set_repetitions(AnimationRepeat::Times(1))
-        .set_duration(AnimationDuration::PerRepetition(200));
-    let animation_id = library.register_animation(animation);
-    library
-        .name_animation(animation_id, Movement::Uppercut)
-        .unwrap();
+    if library.animation_with_name(Movement::Uppercut).is_none() {
+        let uppercut_clip = Clip::from_frames(spritesheet.horizontal_strip(55, 0, 6));
+        let uppercut_clip_id = library.register_clip(uppercut_clip);
+        let mut animation = Animation::from_clip(uppercut_clip_id);
+        animation
+            .set_repetitions(AnimationRepeat::Times(1))
+            .set_duration(AnimationDuration::PerRepetition(200));
+        let animation_id = library.register_animation(animation);
+        library
+            .name_animation(animation_id, Movement::Uppercut)
+            .unwrap();
+    }
 
     let layout = texture_atlas_layouts.add(spritesheet.atlas_layout(32, 32));
 
-    commands.spawn(PlayerBundle {
-        player: Player,
-        name: Name::new("Player"),
-        movement: Movement::Idle,
-        sprite_bundle: Sprite3dBuilder::from_image(texture)
-            .with_atlas(layout)
-            .with_transform(Transform::from_xyz(1., 4., 1.))
-            .with_custom_size(Vec2::new(1., 1.))
-            .build(),
-        sprite_sheet_animation: SpritesheetAnimation::from_id(idle_animation_id),
-        collider_bundle: ColliderBundle {
-            collider: Collider::round_cylinder(0.4, 0.1, 0.1),
-            rigid_body: RigidBody::KinematicPositionBased,
-            active_events: ActiveEvents::COLLISION_EVENTS,
-            ..default()
-        },
-        character_controller: KinematicCharacterController {
-            custom_mass: Some(10.0),
-            up: Vec3::Y,
-            offset: CharacterLength::Absolute(0.01),
-            slide: true,
-            autostep: Some(CharacterAutostep {
-                max_height: CharacterLength::Relative(0.3),
-                min_width: CharacterLength::Relative(0.5),
-                include_dynamic_bodies: false,
-            }),
-            // Don’t allow climbing slopes larger than 45 degrees.
-            max_slope_climb_angle: 45.0_f32.to_radians(),
-            // Automatically slide down on slopes smaller than 30 degrees.
-            min_slope_slide_angle: 30.0_f32.to_radians(),
-            apply_impulse_to_dynamic_bodies: true,
-            snap_to_ground: Some(CharacterLength::Absolute(0.5)),
-            ..default()
-        },
-        input_manager: InputManagerBundle {
-            input_map: Input::player_one(),
-            ..default()
-        },
-        velocity: Velocity::default(),
-    });
+    commands
+        .spawn(PlayerBundle {
+            player: Player,
+            name: Name::new("Player"),
+            movement: Movement::Idle,
+            sprite_bundle: Sprite3dBuilder::from_image(texture)
+                .with_atlas(layout)
+                .with_transform(Transform::from_xyz(1., 4., 1.))
+                .with_custom_size(Vec2::new(2., 2.))
+                .build(),
+            sprite_sheet_animation: SpritesheetAnimation::from_id(idle_animation_id),
+            collider_bundle: ColliderBundle {
+                collider: Collider::round_cylinder(0.9, 0.05, 0.1),
+                rigid_body: RigidBody::KinematicPositionBased,
+                active_events: ActiveEvents::COLLISION_EVENTS,
+                ..default()
+            },
+            character_controller: KinematicCharacterController {
+                custom_mass: Some(10.0),
+                up: Vec3::Y,
+                offset: CharacterLength::Absolute(0.01),
+                slide: true,
+                autostep: Some(CharacterAutostep {
+                    max_height: CharacterLength::Relative(0.3),
+                    min_width: CharacterLength::Relative(0.5),
+                    include_dynamic_bodies: false,
+                }),
+                // Don’t allow climbing slopes larger than 45 degrees.
+                max_slope_climb_angle: 45.0_f32.to_radians(),
+                // Automatically slide down on slopes smaller than 30 degrees.
+                min_slope_slide_angle: 30.0_f32.to_radians(),
+                apply_impulse_to_dynamic_bodies: true,
+                snap_to_ground: Some(CharacterLength::Absolute(0.5)),
+                ..default()
+            },
+            input_manager: InputManagerBundle {
+                input_map: Input::player_one(),
+                ..default()
+            },
+            velocity: Velocity::default(),
+        })
+        .with_children(|children| {
+            children.spawn((
+                Collider::cuboid(0.2, 0.2, 0.2),
+                TransformBundle::from(Transform::from_xyz(0.2, 0., 0.)),
+                Sensor,
+            ));
+        });
 }
 
 pub fn despawn(mut commands: Commands, player_query: Query<Entity, With<Player>>) {
     if let Ok(player) = player_query.get_single() {
-        commands.entity(player).despawn();
+        commands.entity(player).despawn_recursive();
     }
 }
 
 const DELTA: f32 = 0.5;
 
-pub fn change_player_animation(
+pub fn flip_player_sprite(
     library: Res<AnimationLibrary>,
     mut query: Query<
         (
@@ -304,11 +330,60 @@ pub fn move_player(
         }
 
         if is_moving {
-            let translation_change = velocity * time.delta_seconds();
+            let mut translation_change = velocity * time.delta_seconds();
             controller.translation = match controller.translation {
-                Some(existing_translation) => Some(existing_translation + translation_change),
-                None => Some(translation_change),
+                Some(existing_translation) => {
+                    translation_change += existing_translation;
+                    debug!("existing {existing_translation} updated: {translation_change}");
+                    Some(translation_change)
+                }
+                None => {
+                    debug!("{translation_change}");
+                    Some(translation_change)
+                }
             };
+        }
+    }
+}
+
+pub fn punch(
+    character_controller_outputs: Query<
+        (
+            &ActionState<Input>,
+            &KinematicCharacterControllerOutput,
+            &Velocity,
+        ),
+        (With<Player>, Changed<KinematicCharacterControllerOutput>),
+    >,
+    mut enemy_impulses: Query<&mut ExternalImpulse, With<Enemy>>,
+) {
+    if let Ok((input, output, player_velocity)) = character_controller_outputs.get_single() {
+        if input.just_pressed(&Input::Hook) {
+            for collision in &output.collisions {
+                if let Ok(mut ext_impulse) = enemy_impulses.get_mut(collision.entity) {
+                    if player_velocity.linvel.x > DELTA {
+                        ext_impulse.impulse = Vec3::new(0., 0., -0.2);
+                    } else if player_velocity.linvel.x < -DELTA {
+                        ext_impulse.impulse = Vec3::new(0., 0., 0.2);
+                    }
+                }
+            }
+        } else if input.just_pressed(&Input::LightPunch) {
+            for collision in &output.collisions {
+                if let Ok(mut ext_impulse) = enemy_impulses.get_mut(collision.entity) {
+                    if player_velocity.linvel.x > DELTA {
+                        ext_impulse.impulse = Vec3::new(0.2, 0., 0.);
+                    } else if player_velocity.linvel.x < -DELTA {
+                        ext_impulse.impulse = Vec3::new(-0.2, 0., 0.);
+                    }
+                }
+            }
+        } else if input.just_pressed(&Input::Uppercut) {
+            for collision in &output.collisions {
+                if let Ok(mut ext_impulse) = enemy_impulses.get_mut(collision.entity) {
+                    ext_impulse.impulse = Vec3::new(0., 0.1, 0.);
+                }
+            }
         }
     }
 }
@@ -324,7 +399,6 @@ pub fn collect_fruits(
     if let Ok(output) = character_controller_outputs.get_single() {
         for collision in &output.collisions {
             if fruits.get(collision.entity).is_ok() {
-                info!("Fruit collected");
                 commands.entity(collision.entity).despawn()
             }
         }
