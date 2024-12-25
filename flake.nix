2bbba8a94c0e6f49ff5a18ce4ef4b21475012a25
Marijan Petričević <marijan.petricevic94@gmail.com>
2024-12-25 12:23:52 +0100

    flake/wasm: use correct wasm-bindgen version

--- flake.nix
+++ flake.nix
@@ -144,7 +144,16 @@
             );
 
             game-dist =
-              pkgs.runCommand "game-wasm-bindgen" { nativeBuildInputs = [ pkgs.wasm-bindgen-cli ]; }
+              pkgs.runCommand "game-wasm-bindgen"
+                {
+                  nativeBuildInputs = [
+                    (pkgs.wasm-bindgen-cli.override {
+                      version = "0.2.99";
+                      hash = "sha256-1AN2E9t/lZhbXdVznhTcniy+7ZzlaEp/gwLEAucs6EA=";
+                      cargoHash = "sha256-DbwAh8RJtW38LJp+J9Ht8fAROK9OabaJ85D9C/Vkve4=";
+                    })
+                  ];
+                }
                 ''
                   wasm-bindgen --no-typescript --target web \
                     --out-dir $out \
